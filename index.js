d97b9eb7d17a3c73f3a269060a1c5cebfbcf1698
Priyanshu921 <priyanshupatidar@yahoo.com>
2023-12-03 15:07:08 +0530

    testing websocket removed

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ mongoose
   .then(async() => {
     const server = app.listen(port);
     const io = socket.io(server);
-    io.on('connection',(socket)=>{
-      console.log("socket connection running on port, "+socket)
-    })
     console.log("Server running on port: 3001");
   })
   .catch((error) => {
