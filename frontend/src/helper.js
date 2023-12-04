a95866e0b4f55a64ffb6aee081d1be83e929063b
Priyanshu921 <priyanshupatidar@yahoo.com>
2023-12-04 15:51:44 +0530

    changed websocket url

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV !== "production"?"http://localhost:3001/api/":"/api/"
-export const BASE_URL_WEBSOCKET = process.env.NODE_ENV !== "production"?"ws://localhost:3001/":"ws://thewhitediary.onrender.com/"
+export const BASE_URL_WEBSOCKET = process.env.NODE_ENV !== "production"?"ws://localhost:3001/":"wss://thewhitediary.onrender.com/"
 export const postTextLength = 300;
