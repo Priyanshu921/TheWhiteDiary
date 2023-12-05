4a7a922b06b1ced4a7183945135f8e12631f754d
Priyanshu921 <priyanshupatidar@yahoo.com>
2023-12-06 00:17:33 +0530

    changed the User Logo to be centerd always

--- frontend/src/components/utils/NameInitials.jsx
+++ frontend/src/components/utils/NameInitials.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import nameInitCSS from './NameInitials.module.scss'
 const NameInitials = (props) => {
   return (
-    <span className={`rounded-circle mx-1 ${nameInitCSS.userNameInitials}`}>
-      {props.nameInitials}
-    </span>
+    <div className={`rounded-circle mx-1 ${nameInitCSS.userNameInitials}`}>
+      <span>{props.nameInitials}</span>
+    </div>
   );
 }
 
